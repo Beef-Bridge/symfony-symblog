2682162dcefdd1d0429a3f7872ac1eea46f4a8cc
Nikoquattro <nikoquattro@gmail.com>
2023-02-14 23:58:59 +0100

    modif conf tailwind

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -3,9 +3,12 @@ module.exports = {
   content: [
     'templates/**/*.html.twig',
     'assets/js/**/*.js',
+    './node_modules/tw-elements/dsit/js/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
 }
