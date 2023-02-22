0755640557985d9cc36c96b12e68ea027d76a6a3
Nikoquattro <nikoquattro@gmail.com>
2023-02-22 22:12:18 +0100

    creation de la methode testReturnToBlogWorks()

--- project/tests/Functional/Post/PostTest.php
+++ project/tests/Functional/Post/PostTest.php
@@ -37,4 +37,35 @@ class PostTest extends WebTestCase
         $this->assertSelectorExists('h1');
         $this->assertSelectorTextContains('h1', ucfirst($post->getTitle()));
     }
+
+    public function testReturnToBlogWorks(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGeneratorInterface */
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+        /** @var EntityManager $em */
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var PostRepository $postRepository */
+        $postRepository = $em->getRepository(Post::class);
+
+        /** @var Post */
+        $post = $postRepository->findOneBy([]);
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGeneratorInterface->generate('post_details', ['slug' => $post->getSlug()])
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $link = $crawler->selectLink('Retour')->link()->getUri();
+
+        $crawler = $client->request(Request::METHOD_GET, $link);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertRouteSame('post_index');
+    }
 }
