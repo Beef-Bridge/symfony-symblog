970bfc0c90482ca6f261381881d7aa456b1c2f8d
Nikoquattro <nikoquattro@gmail.com>
2023-04-12 22:57:38 +0200

    fixtures pour categories_posts

--- project/src/DataFixtures/CategoryFixtures.php
+++ project/src/DataFixtures/CategoryFixtures.php
@@ -3,22 +3,26 @@
 namespace App\DataFixtures;
 
 use App\Entity\Post\Category;
+use App\Repository\PostRepository;
 use Faker\Factory;
 use Faker\Generator;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class CategoryFixtures extends Fixture
+class CategoryFixtures extends Fixture implements DependentFixtureInterface
 {
     private Generator $faker;
 
-    public function __construct()
-    {
+    public function __construct(
+        private PostRepository $postRepository
+    ) {
         $this->faker = Factory::create('fr_FR');
     }
 
     public function load(ObjectManager $manager): void
     {
+        $categories = [];
         for ($i = 0; $i < 10; $i++) {
             $category = new Category();
             $category->setName($this->faker->words(1, true) . ' ' . $i);
@@ -27,8 +31,21 @@ class CategoryFixtures extends Fixture
             );
 
             $manager->persist($category);
+            $categories[] = $category;
+        }
+
+        $posts = $this->postRepository->findAll();
+        foreach ($posts as $post) {
+            $post->addCategory(
+                $categories[mt_rand(0, count($categories) - 1)]
+            );
         }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [ThumbnailPostFixtures::class];
     }
 }
