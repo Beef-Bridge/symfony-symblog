23c4478fdf061f013157085b27a605e3ec663111
Nikoquattro <nikoquattro@gmail.com>
2023-04-18 22:50:09 +0200

    utilisation du trait CategoryTagTrait

--- project/src/Entity/Post/Category.php
+++ project/src/Entity/Post/Category.php
@@ -3,13 +3,11 @@
 namespace App\Entity\Post;
 
 use App\Entity\Post;
-use Cocur\Slugify\Slugify;
+use App\Entity\Trait\CategoryTagTrait;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\Post\CategoryRepository;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\JoinTable;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -20,127 +18,11 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 )]
 class Category
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private ?int $id = null;
-
-    #[ORM\Column(type: 'string', length: 255, unique: true)]
-    #[Assert\NotBlank()]
-    private string $name;
-
-    #[ORM\Column(type: 'string', length: 255, unique: true)]
-    #[Assert\NotBlank()]
-    private string $slug = '';
-
-    #[ORM\Column(type: 'text', nullable: true)]
-    #[Assert\NotBlank()]
-    private ?string $description = null;
-
-    #[ORM\Column(type: 'datetime_immutable')]
-    #[Assert\NotNull()]
-    private \DateTimeImmutable $createdAt;
+    use CategoryTagTrait;
 
     #[ORM\ManyToMany(targetEntity: Post::class, inversedBy: 'categories')]
     #[JoinTable(name: 'categories_posts')]
     private Collection $posts;
-
-    public function __construct()
-    {
-        $this->createdAt = new \DateTimeImmutable();
-        $this->posts = new ArrayCollection();
-    }
-
-    #[ORM\PrePersist]
-    public function prePersist()
-    {
-        $this->slug = (new Slugify())->slugify($this->name);
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    /**
-     * Get the value of name
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set the value of name
-     *
-     * @return  self
-     */
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of slug
-     */
-    public function getSlug(): string
-    {
-        return $this->slug;
-    }
-
-    /**
-     * Set the value of slug
-     *
-     * @return  self
-     */
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of description
-     */
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    /**
-     * Set the value of description
-     *
-     * @return  self
-     */
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of createdAt
-     */
-    public function getCreatedAt(): \DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * Set the value of createdAt
-     *
-     * @return  self
-     */
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
 
     public function getPosts(): Collection
     {
@@ -161,10 +43,5 @@ class Category
         $this->posts->removeElement($post);
 
         return $this;
-    }
-
-    public function __toString()
-    {
-        return $this->name;
     }
 }
