33563ad325a580aa82a2c155a7087399ff03a3f2
Nikoquattro <nikoquattro@gmail.com>
2023-05-14 16:15:49 +0200

    creation de la classe TagTest et de la methode testPageWorks()

---
+++ project/tests/Functional/Tag/TagTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Functional\Tag;
+
+use App\Entity\Post\Tag;
+use App\Repository\Post\TagRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class TagTest extends WebTestCase
+{
+    public function testPageWorks(): void
+    {
+        $client = static::createClient();
+        
+        /** @var UrlGeneratorInterface $urlGeneratorInterface */
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+        /** @var EntityManager $em */
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var TagRepository $tagRepository */
+        $tagRepository = $em->getRepository(Tag::class);
+        /** @var Tag $tag */
+        $tag = $tagRepository->findOneBy([]);
+
+        $client->request(
+            Request::METHOD_GET,
+            $urlGeneratorInterface->generate('tag_index', ['slug' => $tag->getSlug()])
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorExists('h1');
+        $this->assertSelectorTextContains('h1', 'Etiquette : ' . ucfirst($tag->getName()));
+    }
+}
