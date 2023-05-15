9ab86fb89966acca0ef39ae18eaf184e8decabca
Nikoquattro <nikoquattro@gmail.com>
2023-05-15 22:58:32 +0200

    ajout fonction testSearchBarWorks()

--- project/tests/Functional/Blog/BlogTest.php
+++ project/tests/Functional/Blog/BlogTest.php
@@ -2,6 +2,9 @@
 
 namespace App\Tests\Functional\Blog;
 
+use App\Entity\Post;
+use App\Entity\Post\Tag;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -54,5 +57,61 @@ class BlogTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
         $this->assertRouteSame('category_index');
+    }
+
+    public function testSearchBarWorks(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGeneratorInterface */
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+        /** @var EntityManager $em */
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var PostRepository $postRepository */
+        $postRepository = $em->getRepository(Post::class);
+        /** @var Post $post */
+        $post = $postRepository->findOneBy([]);
+        /** @var Tag $tag */
+        $tag = $post->getTags()[0];
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGeneratorInterface->generate('post_index')
+        );
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $searchList = [
+            substr($post->getTitle(), 0, 3),
+            substr($tag->getName(), 0, 3)
+        ];
+
+        foreach ($searchList as $search) {
+            $form = $crawler->filter('form[name=search]')->form([
+                'search[q]' => $search
+            ]);
+
+            $crawler = $client->submit($form);
+
+            $this->assertResponseIsSuccessful();
+            $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+            $this->assertRouteSame('post_index');
+
+            $nbPosts = count($crawler->filter('div.card'));
+            $postsTitle = $crawler->filter('div.card h5');
+            $count = 0;
+
+            foreach ($postsTitle as $title) {
+                if (
+                    str_contains($title->textContent, $search) ||
+                    str_contains($tag->getName(), $search)
+                ) {
+                    $count++;
+                }
+            }
+
+            $this->assertEquals($nbPosts, $count);
+        }
     }
 }
