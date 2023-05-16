58be0594b68be79f50778e5b4eebcf65401fb72b
Nikoquattro <nikoquattro@gmail.com>
2023-05-16 22:13:36 +0200

    application barre de recherche avec filtres sur la page Categories

--- project/src/Controller/CategoryController.php
+++ project/src/Controller/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
+use App\Model\SearchData;
 use App\Entity\Post\Category;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,9 +20,25 @@ class CategoryController extends AbstractController
         Category $category,
         PostRepository $postRepository
     ): Response {
+        $searchData = new SearchData();
+        $form = $this->createForm(SearchType::class, $searchData); 
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $searchData->page = $request->query->getInt('page', 1);
+            $posts = $postRepository->findBySearch($searchData);
+
+            return $this->render('post/index.html.twig', [
+                'form' => $form->createView(),
+                'category' => $category,
+                'posts' => $posts
+            ]);
+        }
+
         $posts = $postRepository->findPublished($request->query->getInt('page', 1), $category);
 
         return $this->render('category/index.html.twig', [
+            'form' => $form->createView(),
             'category' => $category,
             'posts' => $posts
         ]);
