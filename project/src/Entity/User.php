3afee0a033658aefe8e047b7895fef43a1212531
Nikoquattro <nikoquattro@gmail.com>
2023-05-17 22:24:47 +0200

    creation entity/repo User + defition strategy uuid

---
+++ project/src/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue('CUSTOM')]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\CustomIdGenerator('doctrine.uuid_generator')]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
