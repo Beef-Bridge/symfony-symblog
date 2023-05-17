4455a2a4ce06366d097097b9c6502fb7f6f5c7a7
Nikoquattro <nikoquattro@gmail.com>
2023-05-17 22:43:53 +0200

    déclaration propriétés

--- project/src/Entity/User.php
+++ project/src/Entity/User.php
@@ -14,6 +14,32 @@ class User
     #[ORM\CustomIdGenerator('doctrine.uuid_generator')]
     private ?int $id = null;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $avatar;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    private ?string $email;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $lastname = null;
+    
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = ['ROLE_USER'];
+
+    private string $plainPassword = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $passord;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $updateddAt;
+
     public function getId(): ?int
     {
         return $this->id;
