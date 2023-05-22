c328ad63a263493f8380c59ebfd7ec6a6aedb6b0
Nikoquattro <nikoquattro@gmail.com>
2023-05-22 23:11:39 +0200

    petit retour en arrière

--- project/src/Controller/LikeController.php
+++ project/src/Controller/LikeController.php
@@ -7,13 +7,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LikeController extends AbstractController
 {
     #[Route('/like/article/{id}', name: 'like_post', methods: [Request::METHOD_GET])]
-    #[IsGranted('ROLE_USER')]
     public function like(Post $post, EntityManagerInterface $manager): Response
     {
         $user = $this->getUser();
