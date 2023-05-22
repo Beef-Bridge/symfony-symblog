5a594e1dcadfd4d6fe1daaf1135e8af725d1d727
Nikoquattro <nikoquattro@gmail.com>
2023-05-22 22:09:27 +0200

    creation du fichier like.js

---
+++ project/assets/scripts/like.js
@@ -0,0 +1,9 @@
+export default class Like {
+    constructor(likeElements) {
+        this.likeElements = likeElements;
+
+        if (this.likeElements) {
+            console.log(this.likeElements);
+        }
+    }
+}
