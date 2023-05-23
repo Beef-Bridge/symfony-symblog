f50c137865b88d8005ee97380252d57d3f0f0061
Nikoquattro <nikoquattro@gmail.com>
2023-05-23 23:37:53 +0200

    utilisation du form CommentType dans la methode details() du controller

--- project/src/Controller/PostController.php
+++ project/src/Controller/PostController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\SearchType;
+use App\Form\CommentType;
 use App\Model\SearchData;
+use App\Entity\Post\Comment;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -39,9 +42,32 @@ class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/article/{slug}', name: 'post_details', methods: [Request::METHOD_GET])]
-    public function details(Post $post): Response
-    {
-        return $this->render('post/details.html.twig', ['post' => $post]);
+    #[Route('/article/{slug}', name: 'post_details', methods: [Request::METHOD_GET, Request::METHOD_POST])]
+    public function details(
+        Post $post,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        $comment = new Comment();
+        $comment->setPost($post);
+        if($this->getUser()) {
+            $comment->setAuthor($this->getUser());
+        }
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre commentaire a bien été enregistré. Il sera soumis à modération dans les plus brefs délais.');
+
+            return $this->redirectToRoute('post.show', ['slug' => $post->getSlug()]);
+        }
+
+        return $this->render('post/details.html.twig', [
+            'post' => $post,
+            'form' => $form->createView()
+        ]);
     }
 }
