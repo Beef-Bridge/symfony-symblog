a8ead55c5c5bb5f592a6d1a67a1bbbdeac17cc8a
Nikoquattro <nikoquattro@gmail.com>
2023-05-23 20:32:48 +0200

    définition des propriétés

--- project/src/Entity/Post/Comment.php
+++ project/src/Entity/Post/Comment.php
@@ -2,11 +2,30 @@
 
 namespace App\Entity\Comment;
 
+use App\Entity\Post;
+use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\Comment\CommentRepository;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
 {
+    private ?int $id = null;
+
+    private string $content;
+
+    private bool $isApproved = false;
+
+    private User $author;
+
+    private Post $post;
+
+    private DateTimeImmutable $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+    }
 
 }
