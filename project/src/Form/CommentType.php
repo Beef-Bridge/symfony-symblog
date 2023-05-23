f205f52915ee270611e4586e325c503562c66fad
Nikoquattro <nikoquattro@gmail.com>
2023-05-23 23:20:48 +0200

    creation du formulaire dédié aux commentairres

---
+++ project/src/Form/CommentType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('content', TextareaType::class, [
+            'label' => 'Poster un nouveau commentaire',
+            'constraints' => [new NotBlank()]
+        ]);
+    }
+}
